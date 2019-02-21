fe6f8461347bc706c86fb42295a751dfb9ee976b
daiki0386 <d.hosokawa0806@gmail.com>
2019-02-21 14:36:25 +0900

    Message-function2

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -39,7 +39,7 @@ $(function(){
       var html = buildHTML(data);
       $('.messages').append(html);
       $(".messages").animate({scrollTop:$('.messages')[0].scrollHeight});
-      $('.form__message').val('');
+      $('.new_message')[0].reset();
       $(".form__submit").removeAttr("disabled");
     })
 
