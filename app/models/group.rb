165a8a8229db8996fe2f60b3babe80f5e6768da6
daiki0386 <d.hosokawa0806@gmail.com>
2019-02-27 14:18:48 +0900

    Try

--- app/models/group.rb
+++ app/models/group.rb
@@ -6,10 +6,10 @@ class Group < ApplicationRecord
   validates :name, presence: true
 
   def show_last_message
-    if (last_message = messages.last).present?
-      last_message.content? ? last_message.content : '画像が投稿されています'
-    else
-      'まだメッセージはありません。'
-    end
-  end
+  #   if (last_message = messages.last).present?
+  #     last_message.content? ? last_message.content : '画像が投稿されています'
+  #   else
+  #     'まだメッセージはありません。'
+  #   end
+  # end
 end
